0a06d11802206cee5262b4e1450cb64a5f5e0d58
Julie Laffy <julie.laffy@weizmann.ac.il>
2019-08-06 20:02:53 +0300

    updating doc and example data

--- R/infercna.R
+++ R/infercna.R
@@ -5,22 +5,7 @@
     list(min = do.call(pmin, v), max = do.call(pmax, v))
 }
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param v PARAM_DESCRIPTION
-#' @param Min PARAM_DESCRIPTION
-#' @param Max PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
-#' @examples 
-#' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
-#' }
-#' @rdname dipcenter
-#' @export 
-dipcenter = function(v, Min, Max) {
+.dipcenter = function(v, Min, Max) {
     above = v > Min & v > Max
     below = v < Min & v < Max
     normal = !above & !below
@@ -30,27 +15,13 @@ dipcenter = function(v, Min, Max) {
     v
 }
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param cna PARAM_DESCRIPTION
-#' @param ... PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details Correction with diploid cells' <dipcells> CNAs: the boundaries of dipcells CNA values are the boundaries for what should be considered a CNA of 0. Thus, if the boundary is -0.1 and 0.1, then a cell with CNA = -0.5 will be corrected to -0.4 and a cell with CNA value of 1 will be corrected to 0.9.
-#' @examples 
-#' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
-#' }
-#' @rdname dipcorrect
-#' @export 
-dipcorrect = function(cna, ...) {
+.dipcorrect = function(cna, ...) {
     dots = list(...)
     genes = rownames(cna)
     Args = c(list(cna = cna), dots)
     c(Min, Max) %<-% do.call(.diprange, Args)
     n = nrow(cna)
-    cna = t(sapply(1:n, function(i) dipcenter(cna[i, ], Min = Min[i], Max = Max[i])))
+    cna = t(sapply(1:n, function(i) .dipcenter(cna[i, ], Min = Min[i], Max = Max[i])))
     rownames(cna) = genes
     cna
 }
@@ -60,17 +31,13 @@ dipcorrect = function(cna, ...) {
 #' @param m a matrix of genes X cells RNA-seq expression data. The matrix should be row-centered.
 #' @param dipcells a list of two or more character vectors, each containg cell IDs of normal cell types (one cell type per list element). Since these cell types are presumed diploid cells, their CNA values can be used to correct the remaining CNA values. Note that all cell IDs should correspond to column names in <m>. See infercna::dipcells (diploid cells) for an example. Default: NULL
 #' @param window the size of the window to use for the rolling mean. Units are in number of genes. Default: 100
-#' @param range values in <m> above and below this range will be set to values in range. Default: c(-3, 3)
+#' @param range values in <m> above and below this range will be set to values in range. See infercna::clip() for more details. Default: c(-3, 3)
 #' @return a matrix of genes X cells of inferred CNA values. Note that n = (window - 1)/2 genes will be lost from either extremity of the genome (ie. n genes lost at the start of chromosome 1 and at the end of chromosome Y, if the genome in question is H.sapiens.)
-#' @details Correction with diploid cells' <dipcells> CNAs: the boundaries of dipcells CNA values are the boundaries for what should be considered a CNA of 0. Thus, if the boundary is -0.1 and 0.1, then a cell with CNA = -0.5 will be corrected to -0.4 and a cell with CNA value of 1 will be corrected to 0.9.
+#' @details Correction with diploid cells' <dipcells> CNAs: the boundaries of dipcells CNA values are the boundaries for what should be considered a CNA of 0. Thus, if the boundary is -0.1 and 0.1, then a cell with CNA = -0.5 will be corrected to -0.4, a cell with CNA value of 1 will be corrected to 0.9 and a cell with CNA value of 0.05 will be corrected to 0.
 #' @examples 
-#' \dontrun{
-#' if(interactive()){
 #'  dipcells = infercna::dipcells
 #'  m = infercna::useData()
 #'  cna = infercna::infercna(m = m, dipcells = dipcells)
-#'  }
-#' }
 #' @rdname infercna
 #' @export 
 infercna = function(m,
@@ -79,13 +46,13 @@ infercna = function(m,
                     range = c(-3, 3)) {
 
     # note: m should be row(gene)-centered
-    cna = genorder(m)
+    cna = orderGenes(m)
     cna = clip(cna, range = range)
     cna = runMean(cna, k = window)
     cna = colCenter(cna)
     if (!is.null(dipcells)) {
         Args = c(list(cna = cna), dipcells)
-        cna = do.call(dipcorrect, Args)}
+        cna = do.call(.dipcorrect, Args)}
     cna
 }
 
--- R/utils-infercna.R
+++ R/utils-infercna.R
@@ -1,18 +1,5 @@
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param genes PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
-#' @examples 
-#' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
-#' }
-#' @rdname genmatch
-#' @export 
-genmatch = function(genes) {
+.geneMatch = function(genes) {
     match(g.env$gene, genes, nomatch = 0)
 }
 
@@ -27,54 +14,50 @@ genmatch = function(genes) {
 #'  #EXAMPLE1
 #'  }
 #' }
-#' @rdname as_genomic
+#' @rdname orderGenes 
 #' @export 
-as_genomic = function(x = NULL) {
-    if (is.null(dim(x))) return(x[genmatch(genes = x)])
-    x[genmatch(rownames(x)), , drop = FALSE]
+orderGenes = function(x = NULL) {
+    if (is.null(dim(x))) return(x[.geneMatch(genes = x)])
+    x[.geneMatch(rownames(x)), , drop = FALSE]
 }
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param m PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
-#' @examples 
-#' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
-#' }
+#' @title Center a matrix column-wise 
+#' @description Center a matrix column-wise 
+#' @param m a matrix
+#' @return a matrix with colMeans == 0
 #' @rdname colCenter
 #' @export 
 colCenter = function(m) {
     scale(m, center = T, scale = F)
 }
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param mat PARAM_DESCRIPTION
-#' @param range PARAM_DESCRIPTION, Default: c(-3, 3)
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
-#' @examples 
-#' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
-#' }
+
+#' @title Center a matrix row-wise 
+#' @description Center a matrix row-wise 
+#' @param m a matrix
+#' @return a matrix with rowMeans == 0
+#' @rdname rowCenter
+#' @export 
+rowCenter = function(m) {
+    t(scale(t(m), center = T, scale = F))
+}
+
+#' @title Squish matrix values into range
+#' @description Squish matrix values into range
+#' @param m matrix to manipulate
+#' @param range numeric vector of length two giving desired output range. Default: c(-3, 3)
 #' @rdname clip
 #' @export 
-clip <- function(mat, range = c(-3, 3)) {
-    mat = as.matrix(mat)
-    mat[mat < range[[1]]] <- range[[1]]
-    mat[mat > range[[2]]] <- range[[2]]
-    mat
+clip <- function(m, range = c(-3, 3)) {
+    m = as.matrix(m)
+    m[m < range[[1]]] <- range[[1]]
+    m[m > range[[2]]] <- range[[2]]
+    m
 }
 
 #' @title FUNCTION_TITLE
 #' @description FUNCTION_DESCRIPTION
-#' @param mat PARAM_DESCRIPTION
+#' @param m PARAM_DESCRIPTION
 #' @param k PARAM_DESCRIPTION, Default: 100
 #' @param endrule PARAM_DESCRIPTION, Default: 'mean'
 #' @param align PARAM_DESCRIPTION, Default: 'center'
@@ -91,18 +74,18 @@ clip <- function(mat, range = c(-3, 3)) {
 #' @rdname runMean
 #' @export 
 #' @importFrom caTools runmean
-runMean <- function(mat,
+runMean <- function(m,
                     k = 100,
                     endrule = 'mean',
                     align = 'center') {
 
-    mat = as.matrix(mat)
-    mat.out = caTools::runmean(mat,
-                               k = k,
-                               endrule = endrule,
-                               align = align)
-    colnames(mat.out) <- colnames(mat)
-    rownames(mat.out) <- rownames(mat)
-    as.data.frame(mat.out)
+    m = as.matrix(m)
+    mout = caTools::runmean(m,
+                            k = k,
+                            endrule = endrule,
+                            align = align)
+    colnames(mout) = colnames(m)
+    rownames(mout) = rownames(m)
+    as.data.frame(mout)
 }
 

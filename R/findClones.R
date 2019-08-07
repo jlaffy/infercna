89fea4e68c0c3e52ee5a48f07102b3d3a8cf1c9d
Julie Laffy <julie.laffy@weizmann.ac.il>
2019-08-07 05:00:59 +0300

    updated findClones

--- R/findClones.R
+++ R/findClones.R
@@ -1,37 +1,22 @@
-#' @title Find Genetic Clones
-#' @description FUNCTION_DESCRIPTION
-#' @param m PARAM_DESCRIPTION, Default: NULL
-#' @param L PARAM_DESCRIPTION, Default: NULL
-#' @param by PARAM_DESCRIPTION, Default: 'arm'
-#' @param prob PARAM_DESCRIPTION, Default: 0.95
-#' @param coverage PARAM_DESCRIPTION, Default: 0.8
-#' @param size PARAM_DESCRIPTION, Default: 10
-#' @param boolean PARAM_DESCRIPTION, Default: FALSE
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
-#' @examples 
-#' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
-#' }
-#' @rdname findClones
-#' @export 
-findClones = function(m, prob = 0.95, rate = 0.8, size = 10) {
 
-    .fetchData = function(m, splitBy) {
-        mats = splitGenes(m, by = splitBy)
-        modes = sapply(mats, fitBimodal, assign = T)
-        modes[!sapply(modes, isFALSE)]}
+fetchModes = function(m, prob = 0.95, coverage = 0.8, size = 10, by = 'chr') {
+    mats = splitGenes(m, by = by)
+    modes = sapply(mats, fitBimodal, prob = prob, coverage = coverage, size = size, assign = T)
+    modes[!sapply(modes, isFALSE)]
+}
 
-    armModes = .fetchData(m, splitBy = 'arm')
-    chrModes = .fetchData(m, splitBy = 'chr')
+expandToClones = function(modes, greaterThan = NULL) {
+    modes = sapply(modes, function(x) {
+                       stats::setNames(unlist(x), rep(names(x), lengths(x)))}, simplify = F)
+    modes = unlist(modes)
+    clones = sapply(split(names(modes), modes), paste0, collapse = '--')
+    clones = split(names(clones), clones)
+    if (is.null(greaterThan)) return(clones)
+    clones[lengths(clones) > greaterThan]
+}
 
 
-
-    modemat = modemat[rowSums(modemat) != 0, colSums(modemat) != 0, drop = FALSE]
-    gr1 = apply(modemat, 1, function(r) paste0(colnames(modemat)[which(r == 1)], collapse = '_'))
-    gr2 = apply(modemat, 1, function(r) paste0(colnames(modemat)[which(r == 2)], collapse = '_'))
-    cloneIDs = mapply(function(x, y) paste(x, y, sep = '-'), x = gr1, y = gr2)
-    split(names(cloneIDs), cloneIDs)
+findClones = function(m, prob = 0.95, coverage = 0.8, mode.size = 10, clone.size = NULL, by = 'chr') {
+    modes = fetchModes(m, prob = prob, coverage = coverage, size = mode.size, by = by)
+    expandToClones(modes, greaterThan = clone.size)
 }

10ecb678a87165468dfe3884d385f95293603ea0
Julie Laffy <julie.laffy@weizmann.ac.il>
2019-08-16 09:23:38 +0100

    updating doc

--- R/findClones.R
+++ R/findClones.R
@@ -1,13 +1,23 @@
 
-fetchModes = function(m, prob = 0.95, coverage = 0.8, size = 10, by = 'chr') {
+fetchModes = function(m,
+                      prob = 0.95,
+                      coverage = 0.8,
+                      size = 10,
+                      by = 'chr') {
+
     mats = splitGenes(m, by = by)
     modes = sapply(mats, fitBimodal, prob = prob, coverage = coverage, size = size, assign = T)
     modes[!sapply(modes, isFALSE)]
 }
 
-expandToClones = function(modes, greaterThan = NULL) {
+
+expandToClones = function(modes,
+                          greaterThan = NULL) {
+
     modes = sapply(modes, function(x) {
-                       stats::setNames(unlist(x), rep(names(x), lengths(x)))}, simplify = F)
+                       stats::setNames(unlist(x), rep(names(x), lengths(x)))},
+                       simplify = F)
+
     modes = unlist(modes)
     clones = sapply(split(names(modes), modes), paste0, collapse = '--')
     clones = split(names(clones), clones)
@@ -16,7 +26,18 @@ expandToClones = function(modes, greaterThan = NULL) {
 }
 
 
-findClones = function(m, prob = 0.95, coverage = 0.8, mode.size = 10, clone.size = NULL, by = 'chr') {
-    modes = fetchModes(m, prob = prob, coverage = coverage, size = mode.size, by = by)
+findClones = function(m,
+                      prob = 0.95,
+                      coverage = 0.8,
+                      mode.size = 10,
+                      clone.size = NULL,
+                      by = 'chr') {
+
+    modes = fetchModes(m,
+                       prob = prob,
+                       coverage = coverage,
+                       size = mode.size,
+                       by = by)
+
     expandToClones(modes, greaterThan = clone.size)
 }

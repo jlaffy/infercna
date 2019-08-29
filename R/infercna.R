0e7f73c565ca106c05fb8cc32a3519784b6430ca
Julie Laffy <julie.laffy@weizmann.ac.il>
2019-08-29 10:57:26 +0300

    moving refCorrect function to sep file

--- R/infercna.R
+++ R/infercna.R
@@ -13,49 +13,6 @@
     m[rownames(m) %in% hq, ]
 }
 
-
-.refrange = function(cna, ...) {
-    dots = list(...)
-    v = sapply(dots, function(ref) rowMeans(cna[, ref, drop = F]), simplify = F)
-    list(Min = do.call(pmin, v), Max = do.call(pmax, v))
-}
-
-.refcenter = function(v, Min, Max, noise = NULL) {
-    if (!is.null(noise)) {
-        Min = Min - noise
-        Max = Max + noise
-    }
-    above = v > Min & v > Max
-    below = v < Min & v < Max
-    normal = !above & !below
-    v[above] <- v[above] - Max
-    v[below] <- v[below] - Min
-    v[normal] <- 0
-    v
-}
-
-#' @title Convert Relative CNA Values To Absolute
-#' @description If the identities of normal cells are known, the expected CNA values of these cells should be 0. Thus, CNA values within the range of normal cell CNA values are corrected to 0; CNA values below the range have the minimum substracted; CNA values above the range have the maximum subtracted.
-#' @param cna a matrix of rows (genes) by columns (cells) of CNA values. 
-#' @param noise a numeric value, which if given, increases the boundaries within which CNA values are considered 0. Increases by <noise> i.e. the bounds become Minimum - noise and Maximum + noise. Default: NULL
-#' @param ... normal cell column IDs. Expects at least two character vectors. 
-#' @rdname refCorrect
-#' @export 
-refCorrect = function(cna, noise = NULL, ...) {
-    dots = list(...)
-    genes = rownames(cna)
-    Args = c(list(cna = cna), dots)
-    rg = do.call(.refrange, Args)
-    n = nrow(cna)
-    cna = t(sapply(1:n, function(i) {
-                       .refcenter(cna[i, ],
-                                  Min = rg$Min[i],
-                                  Max = rg$Max[i],
-                                  noise = noise)}))
-    rownames(cna) = genes
-    cna
-}
-
 #' @title Infer Copy-Number Alterations From Single-Cell RNA-Seq Data
 #' @description Infer Copy-Number Alterations From Single-Cell RNA-Seq Data
 #' @param m a matrix of genes X cells RNA-seq expression data. The matrix should be row-centered.
